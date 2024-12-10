7b28cc7b9c2ebea6bb1715d739c5e80eaf1548d1
João Vítor <joaovitorramalho13@gmail.com>
2024-12-10 16:51:16 -0300

    Adicionando Atividade 8 feita

--- Atividade8/eventos.js
+++
@@ -1,4 +0,0 @@
-import troca from "./funcoes.js";
-
-const CardCostas1 = document.getElementById('carta1');
-CardCostas1.addEventListener('click', troca);
---
+++ Atividade8/script.js
@@ -0,0 +1,20 @@
+const cartasSection1 = document.querySelectorAll('#section1 img');
+const cartasSection2 = document.querySelectorAll('#section2 img');
+
+cartasSection2.forEach((carta) => {
+  carta.addEventListener('click', () => {
+    const cartaAtual = Array.from(cartasSection1).find((carta) => 
+      carta.src.includes('images.jpg')
+    );
+    if (cartaAtual) {
+      cartaAtual.src = carta.src;
+    } 
+  });
+});
+
+
+cartasSection1.forEach((carta) => {
+  carta.addEventListener('click', () => {
+    carta.src = './images.jpg';
+  });
+});
