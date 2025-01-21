441b666dd4a0dca978fc1ddaf2c5e424f2ab2d36
JOAO VITOR RAMALHO GONCALVES <0048613@IFMG.EDU.BR>
2025-01-21 10:07:35 -0300

    Adicionando a atividade 11

--- 13-01/index.php
+++ 13-01/index.php
@@ -1,6 +1,23 @@
 <?php
 require_once './ControleCarta.php';
 
-$controle = new ControleCarta();
+$uri = explode('/', $_SERVER['REQUEST_URI']);
+
+$classeControle = 'Controle' . ucfirst($uri[2]);
+$metodo = $uri[3];
+
+$saida['status'] = 'Sem ação';
+
+if (class_exists($classeControle)) {
+    $objetoControle = new $classeControle();
+    if (method_exists($classeControle, $metodo)) {
+        $saida = $objetoControle->$metodo();
+    }
+}
+
+
+echo json_encode($saida);
+/* $controle = new ControleCarta();
 $controle->inserir();
-var_dump($controle->listar()) ;
+
+var_dump($controle->listar()); */
